bbb599d8a54ad6da40bda8b0d32447bac6b71279
Robert Colin <cfflymolo@gmail.com>
2012-11-28 00:33:00 -0800

    Added Storyboard and ViewController for Log In page

---
+++ CARDLoginViewController.m
@@ -0,0 +1,38 @@
+//
+//  CARDLoginViewController.m
+//  TherapistTimeEntry
+//
+//  Created by Robert Colin on 11/28/12.
+//  Copyright (c) 2012 Center for Autism and Related Disorders, Inc. All rights reserved.
+//
+
+#import "CARDLoginViewController.h"
+
+@interface CARDLoginViewController ()
+
+@end
+
+@implementation CARDLoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
