6c935f7c6c8d2ba9539791448b34586d1de848da
Robert Colin <cfflymolo@gmail.com>
2012-11-28 00:24:46 -0800

    Initial Commit

---
+++ TherapistTimeEntry/CARDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CARDAppDelegate.h
+//  TherapistTimeEntry
+//
+//  Created by Robert Colin on 11/28/12.
+//  Copyright (c) 2012 Center for Autism and Related Disorders, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CARDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TherapistTimeEntry/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TherapistTimeEntry
+//
+//  Created by Robert Colin on 11/28/12.
+//  Copyright (c) 2012 Center for Autism and Related Disorders, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CARDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CARDAppDelegate class]));
+    }
+}
