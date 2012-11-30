d9c0d9186d03a4444e8e4c013086ff57e70198fe
Robert Colin <cfflymolo@gmail.com>
2012-11-30 10:36:53 -0800

    Added the CARDOfficeAddressItem to the Profile group

---
+++ CARDOfficeAddress.h
@@ -0,0 +1,23 @@
+//
+//  CARDOfficeAddress.h
+//  TherapistTimeEntry
+//
+//  Created by Robert Colin on 11/30/12.
+//  Copyright (c) 2012 Center for Autism and Related Disorders, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "JSONSerializable.h"
+
+@interface CARDOfficeAddress : NSObject <JSONSerializable>
+
+@property (nonatomic, strong) NSString *addressType;
+@property (nonatomic, strong) NSString *city;
+@property (nonatomic, strong) NSNumber *latitude;
+@property (nonatomic, strong) NSNumber *longitude;
+@property (nonatomic, strong) NSString *addressOne;
+@property (nonatomic, strong) NSString *addressTwo;
+@property (nonatomic, strong) NSString *state;
+@property (nonatomic, strong) NSNumber *zipCode;
+
+@end
---
+++ CARDOfficeAddress.m
@@ -0,0 +1,25 @@
+//
+//  CARDOfficeAddress.m
+//  TherapistTimeEntry
+//
+//  Created by Robert Colin on 11/30/12.
+//  Copyright (c) 2012 Center for Autism and Related Disorders, Inc. All rights reserved.
+//
+
+#import "CARDOfficeAddress.h"
+
+@implementation CARDOfficeAddress
+
+- (void)readFromJSONDictionary:(NSDictionary *)d
+{
+    [self setAddressType:[d objectForKey:@"addressType"]];
+    [self setCity:[d objectForKey:@"city"]];
+    [self setLatitude:[d objectForKey:@"latitude"]];
+    [self setLongitude:[d objectForKey:@"longitude"]];
+    [self setAddressOne:[d objectForKey:@"line1"]];
+    [self setAddressTwo:[d objectForKey:@"line2"]];
+    [self setState:[d objectForKey:@"state"]];
+    [self setZipCode:[d objectForKey:@"zipCode"]];
+}
+
+@end
